1526e3d62a1a60be30b0aff72b64020412a7d3fb
Robin <34440972+Robin-Pooppillikudiyil@users.noreply.github.com>
2018-07-05 16:40:15 +0530

    Updte Tdd

--- Tdd.java
+++ Tdd.java
@@ -1,10 +1,10 @@
 public class Tdd{
 	public static void main(String[] args)
 	{
-<<<<<<< HEAD
+
 		System.out.println("Hello world");
 		System.out.println("welcome";)
-=======
+
 		System.out.println("Hello world1");
 		System.out.println("Hello world2");
 		System.out.println("Hello world3");
@@ -12,7 +12,6 @@ public class Tdd{
 		System.out.println("Hello world5");
 		System.out.println("Hello world6");
 		
-		
->>>>>>> 1dc91bdf5d80f56455a61ca80e2ef089249c2b75
+
 	}
 }
