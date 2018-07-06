205ff10088297c6bb6aff76cb65ab6c0e14d2d68
Robin Thomas <robin.thomas@lxisoft.com>
2018-07-06 12:19:02 +0530

    Tdd changed

--- Tdd.java
+++ Tdd.java
@@ -2,16 +2,9 @@ public class Tdd{
 	public static void main(String[] args)
 	{
 
-		System.out.println("Hello world");
-		System.out.println("welcome";)
-
-		System.out.println("Hello world1");
-		System.out.println("Hello world2");
-		System.out.println("Hello world3");
-		System.out.println("Hello world4");
-		System.out.println("Hello world5");
-		System.out.println("Hello world6");
-		
-
+		for(int i=0;i<10;i++)
+		{		
+			System.out.println(i);
+		}		
 	}
 }
