f9feeebe083c59252e521487320082673f144bff
Prasad-vg <prasad.v.g@lxisoft.com>
2018-07-06 09:30:49 +0530

    modified

--- Tdd.java
+++ Tdd.java
@@ -1,18 +1,12 @@
 public class Tdd{
 	public static void main(String[] args)
 	{
-<<<<<<< HEAD
-		System.out.println("Hello world");
-		System.out.println("welcome";)
-=======
 		System.out.println("Hello world1");
 		System.out.println("Hello world2");
 		System.out.println("Hello world3");
 		System.out.println("Hello world4");
 		System.out.println("Hello world5");
 		System.out.println("Hello world6");
-		
-		
->>>>>>> 1dc91bdf5d80f56455a61ca80e2ef089249c2b75
+
 	}
 }
