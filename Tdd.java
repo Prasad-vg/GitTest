70ebd3c3b189f8391b5f767bde9bce7a53cea7e0
abhina <abhina.s@lxisoft.com>
2018-07-09 10:12:19 +0530

    second commit

---
+++ Tdd.java
@@ -0,0 +1,27 @@
+public class Tdd{
+	public static void main(String[] args)
+	{
+
+
+		System.out.println("Hello world");
+		System.out.println("welcome";)
+
+		System.out.println("Hello world1");
+		System.out.println("Hello world2");
+		System.out.println("Hello world3");
+		System.out.println("Hello world4");
+		System.out.println("Hello world5");
+
+		System.out.println("Hello world6");
+	
+		
+		
+		
+
+	}
+}
+
+		System.out.println("Hello world6");		
+     }
+}
+
